f620e4c55b64de27fcb88c11a58452c8304f9ec6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-03 16:00:56 +0200

    Added docker for continous development

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.use('/api/post', post)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () =>
     console.log(
         `Server is running in ${process.env.NODE_ENV} mode at ${PORT}`.yellow
